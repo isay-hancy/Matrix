e1eee104c98e386b160a25a002966a5782da7ffd
isay-hancy <aishakarki11111@gmail.com>
2026-07-06 17:06:21 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[2, 3];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.WriteLine("Enter elements for row " + i + ":");
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                matrix[i, j] = n;
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Matrix:");
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
